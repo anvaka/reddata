3e3e1f88cc2dcd5479d6703d4cb98a637b957516
anvaka <anvaka@gmail.com>
2015-07-23 02:06:11 -0700

    Formatting

--- recommend.js
+++ recommend.js
@@ -1,6 +1,7 @@
 // Given we have constructed the data base with index.js, this script will
 // recommend similar subreddits for a given one
 var sourceSubreddit = process.argv[2];
+var limit = 10;
 if (!sourceSubreddit) {
   console.log('Pass subreddit name as an argument to get recommendations');
   process.exit(-1);
@@ -11,7 +12,15 @@ redisClient.getRelated(sourceSubreddit)
  .then(print);
 
 function print(subs) {
-  console.log('Users who posted to ' + sourceSubreddit + ' also post to:');
-  console.log(subs);
+  console.log('### related to ' + link(sourceSubreddit));
+  console.log(subs.slice(0, limit).map(toMarkdown).join('\n'));
   process.exit(0);
+}
+
+function toMarkdown(record) {
+  return '* ' + link(record.name) + ' - simlarity ' + record.sim.toFixed(4);
+}
+
+function link(subName) {
+   return '[/r/' + subName + '](https://www.reddit.com/r/' + subName + ')';
 }
